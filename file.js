84c9d207a2a6f7c5b41a03a068715d4e9d199ec3
Tyron-Christian <chriistudio@gmail.com>
2021-11-10 14:48:39 +0100

    Versione 2.0
    Sistemato per il momento solo il lato mobile. Inseriti i box nascosti e i pulsanti funzionanti con il file Javascript.
    Manca il box arcade. Sto caricando il file online

---
+++ file.js
@@ -0,0 +1,12 @@
+// mostriamo le pagine nascoste
+
+
+function mostra(){
+    let boxhidden= document.getElementById('link');
+    boxhidden.style.display = "Flex";
+};
+
+function mostraplay(){
+    let boxhidden= document.getElementById('linkplay');
+    boxhidden.style.display = "Flex";
+};
